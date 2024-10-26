b6db431b4c204682001d6c624be62fd329719db7
davidboers <kwmmineccraft@gmail.com>
2024-10-26 02:18:05 -0400

    Aggregates at the state level

--- temp-2024/scramble.py
+++ temp-2024/scramble.py
@@ -5,6 +5,8 @@ import random
 
 random.seed(201636415)
 
+candidate_agg = {}
+
 def update_votes(office, data):
     if isinstance(data, dict):
         for key, value in data.items():
@@ -15,11 +17,29 @@ def update_votes(office, data):
                     data[key] = random.randint(10000, 50000)
                 else:
                     data[key] = random.randint(100, 1000)
+                
+                if id in candidate_agg[office].keys():
+                    candidate_agg[office][id] += data[key]
+                else:
+                    candidate_agg[office][id] = data[key]
             else:
                 update_votes(office, value)  # Recursively update nested dictionaries
     elif isinstance(data, list):
         for item in data:
             update_votes(office, item)  # Recursively update items in the list
+
+def update_votes_agg(office, id, data):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == 'votes':
+                id = data['id']
+                if id in candidate_agg[office].keys():
+                    data['votes'] = candidate_agg[office][id]
+            else:
+                update_votes_agg(office, id, value)  # Recursively update nested dictionaries
+    elif isinstance(data, list):
+        for item in data:
+            update_votes_agg(office, id, item)  # Recursively update items in the list
 
 def get_party(office, cnd_id: str):
     file_name = './temp-2024/2024-11-05-collection-' + office + '/combined.json'
@@ -34,15 +54,14 @@ def get_party(office, cnd_id: str):
 
 def scramble_for_office(office):
     pattern = os.path.join('./temp-2024/' + office + '/', '*/counties.json')
-
     files = glob.glob(pattern)
+    candidate_agg[office] = {}
 
     for file_name in files:
 
         with open(file_name, 'r') as file:
             try:
                 data = json.load(file)
-                id = data['id']
             except json.decoder.JSONDecodeError:
                 print('json decode error in ' + file_name)
                 continue
@@ -55,3 +74,15 @@ def scramble_for_office(office):
 scramble_for_office('president')
 scramble_for_office('senate')
 scramble_for_office('governor')
+
+for office in candidate_agg.keys():
+    file_name = './temp-2024/2024-11-05-collection-' + office + '/summaries.json'
+
+    with open(file_name, 'r') as file:
+        data: list = json.load(file)
+
+    for id in candidate_agg[office].keys():
+        update_votes_agg(office, id, data)
+
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=4)
