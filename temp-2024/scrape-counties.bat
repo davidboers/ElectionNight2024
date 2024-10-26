c1f3cbe40936ba89ab3585ea7795767db0f79f15
davidboers <kwmmineccraft@gmail.com>
2024-10-26 01:09:53 -0400

    Can now read section headers

--- temp-2024/scrape-counties.bat
+++ temp-2024/scrape-counties.bat
@@ -3,11 +3,19 @@ REM Counties
 @echo off
 setlocal enabledelayedexpansion
 
+set "o=office"
+
 for /f "delims= " %%i in (./temp-2024/contest-ids.txt) do (
-    set "n=%%i"
-    set "n=!n:_=:!"
-    if not exist "./temp-2024/%%i/" (mkdir "./temp-2024/%%i")
-    curl "https://www.politico.com/election-data/pebble/results/live/2024-11-05/contests/!n!/counties.json" -o ./temp-2024/%%i/counties.json
+    echo.%%i | find /I "#">nul && (
+        set "o=%%i"
+        set "o=!o:~1!"
+        echo Hello: "!o!"
+    ) || (
+        set "n=%%i"
+        set "n=!n:_=:!"
+        if not exist "./temp-2024/!o!/%%i/" (mkdir "./temp-2024/!o!/%%i")
+        curl "https://www.politico.com/election-data/pebble/results/live/2024-11-05/contests/!n!/counties.json" -o ./temp-2024/!o!/%%i/counties.json
+    )
 )
 
 endlocal
