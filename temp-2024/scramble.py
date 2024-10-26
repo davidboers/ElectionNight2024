edb79c561e2b58329832912fc2abde1195fe6a6e
davidboers <kwmmineccraft@gmail.com>
2024-10-26 11:16:21 -0400

    Fixed state-level aggregator

--- temp-2024/scramble.py
+++ temp-2024/scramble.py
@@ -7,7 +7,7 @@ random.seed(201636415)
 
 candidate_agg = {}
 
-def update_votes(meta_file, office, c_id, data):
+def update_votes(meta_file, c_id, data):
     if isinstance(data, dict):
         for key, value in data.items():
             if key == 'votes':
@@ -17,30 +17,28 @@ def update_votes(meta_file, office, c_id, data):
                     data[key] = random.randint(10000, 100000)
                 else:
                     data[key] = random.randint(100, 1000)
-                
-                if cnd_id in candidate_agg[office][c_id].keys():
-                    candidate_agg[office][c_id][cnd_id] += data[key]
+
+                if cnd_id in candidate_agg[c_id].keys():
+                    candidate_agg[c_id][cnd_id] += data[key]
                 else:
-                    candidate_agg[office][c_id][cnd_id] = data[key]
+                    candidate_agg[c_id][cnd_id] = data[key]
             else:
-                update_votes(meta_file, office, c_id, value)  # Recursively update nested dictionaries
+                update_votes(meta_file, c_id, value)  # Recursively update nested dictionaries
     elif isinstance(data, list):
         for item in data:
-            update_votes(meta_file, office, c_id, item)  # Recursively update items in the list
+            update_votes(meta_file, c_id, item)  # Recursively update items in the list
 
-def update_votes_agg(office, c_id, data):
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if key == 'votes':
-                cnd_id = data['id']
-                if cnd_id in candidate_agg[office][c_id].keys():
-                    data['votes'] = candidate_agg[office][c_id][cnd_id]
-            else:
-                update_votes_agg(office, c_id, value)  # Recursively update nested dictionaries
-    elif isinstance(data, list):
+def update_votes_agg(data):
+    if isinstance(data, list):
         for item in data:
-            update_votes_agg(office, c_id, item)  # Recursively update items in the list
+            c_id = item['id']
+            results = item['results']
 
+            for result in results:
+                cnd_id = result['id']
+                if cnd_id in candidate_agg[c_id].keys():
+                    result['votes'] = candidate_agg[c_id][cnd_id]
+            
 def get_party(summary_file, cnd_id: str):
     with open(summary_file, 'r') as file:
         data: list = json.load(file)
@@ -53,32 +51,30 @@ def get_party(summary_file, cnd_id: str):
 def scramble_for_office(counties_glob_path, meta_file, summary_file, office):
     pattern = os.path.join(counties_glob_path)
     files = glob.glob(pattern)
-    candidate_agg[office] = {}
 
     for file_name in files:
         with open(file_name, 'r') as file:
             try:
                 data = json.load(file)
-                c_id = data['id']                    
-                candidate_agg[office][c_id] = {}
+                c_id = data['id']                
+                candidate_agg[c_id] = {}
             except json.decoder.JSONDecodeError:
                 print('json decode error in ' + file_name)
                 continue
 
-        update_votes(meta_file, office, c_id, data)
+        update_votes(meta_file, c_id, data)
 
         with open(file_name, 'w') as file:
             json.dump(data, file, indent=4)
 
-    for c_id in candidate_agg[office].keys():
+    with open(summary_file, 'r') as file:
+        data: list = json.load(file)
 
-        with open(summary_file, 'r') as file:
-            data: list = json.load(file)
+        contests = data['contests']
+        update_votes_agg(contests)
 
-        update_votes_agg(office, c_id, data)
-
-        with open(summary_file, 'w') as file:
-            json.dump(data, file, indent=4)
+    with open(summary_file, 'w') as file:
+        json.dump(data, file, indent=4)
             
 with open('./temp-2024/scramble-manifest.json', 'r') as manifest_file:
     manifest = json.load(manifest_file)
@@ -90,3 +86,5 @@ with open('./temp-2024/scramble-manifest.json', 'r') as manifest_file:
         office = item['office']
 
         scramble_for_office(county_glob, meta_file, summary_file, office)
+
+#sprint(candidate_agg['bq:az:139'])
